c514a8965705f90491d4c902acd46207175556fb
kypMon <978634806@qq.com>
2022-05-10 01:47:45 +0800

    temp commit

--- container-from-scratch/main.go
+++ container-from-scratch/main.go
@@ -3,12 +3,18 @@ package main
 
 import (
 	`fmt`
+	`io/ioutil`
 	`os`
 	`os/exec`
+	`path/filepath`
+	`strconv`
 	`syscall`
 )
 
-const HOSTNAME string = "HOSTNAME"
+const (
+ 	HOSTNAME = "HOSTNAME"
+ 	HOSTROOTPATH = "/home/temp"
+)
 
 // go run main.go run <cmd> <args>
 func main() {
@@ -55,7 +61,17 @@ func run ()  {
 
 	// create new namespace
 	cmd.SysProcAttr = &syscall.SysProcAttr{
-		Cloneflags: syscall.CLONE_NEWUTS, // UTS namespace => hostname
+		// UTS namespace => hostname
+		// PID namespace => process
+		// NEWUSER => rootless container
+		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS | syscall.CLONE_NEWUSER,
+		Credential: &syscall.Credential{Uid: 0, Gid: 0},
+		UidMappings: []syscall.SysProcIDMap{
+			{containerID: 0, HostID: os.Getuid(), Size: 1},
+		},
+		GidMappings: []syscall.SysProcIDMap{
+			{ContainerID: 0, HostID: os.Getgid(), size: 1},
+		},
 	}
 
 	// trigger cli to running
@@ -67,6 +83,8 @@ func run ()  {
 // bash picked up the hostname before we changed it, so that's why we need to have a child process before running bash
 func child () {
 
+	cg()
+
 	fmt.Printf("RUnning %v\n", os.Args[2:])
 
 	cmd := exec.Command(os.Args[2], os.Args[3:]...)
@@ -74,18 +92,74 @@ func child () {
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 
+	// set hostname
 	setHostname(os.Getenv(HOSTNAME))
+
+	// limit the access using the chroot
+	setRootFS(os.Getenv(HOSTROOTPATH))
+
+	// processs
+	setProcessMount()
+
+	// FS
+	setFSMount()
 
 	// trigger cli to running
 	// we don't create the namespace before we finally run this!
 	// so we need to create the namespace first
 	must(cmd.Run())
+
+	// after execution
+	setProcessUnmount()
+	setFSUnmount()
 }
 
 func setHostname (hostname string) {
 	must(syscall.Sethostname([]byte(hostname)))
 }
 
+func setRootFS (rootpath string) {
+	must(syscall.Chroot(rootpath))
+	must(os.Chdir(rootpath))
+}
+
+func setProcessMount () {
+	must(syscall.Mount("proc", "proc", "proc", 0, ""))
+}
+
+func setProcessUnmount () {
+	must(syscall.Unmount("proc", 0))
+}
+
+func setFSMount () {
+	// source string, target string, fstype string, flags uintptr, data string
+	must(syscall.Mount("something", "mytemp", "tmpfs", 0, ""))
+}
+
+func setFSUnmount () {
+	must(syscall.Unmount("mytemp", 0))
+}
+
+// Cgroup
+func cg () {
+	cgroups := "/sys/fs/cgroup/"
+
+	mem := filepath.Join(cgroups, "memory")
+	os.Mkdir(filepath.Join(mem, "ubuntu"), 0755)
+	// memory limit
+	must(ioutil.WriteFile(filepath.Join(mem, "ubuntu/memory.limit_in_bytes"), []byte("999424"), 0700))
+	// swap memory
+	must(ioutil.WriteFile(filepath.Join(mem, "ubuntu/memory.memsw.limit_in_bytes"), []byte("999424"), 0700))
+	// tidy up no longer needed
+	must(ioutil.WriteFile(filepath.Join(mem, "ubuntu/memory.notify_on_release"), []byte("999424"), 0700))
+
+	// get the current process id and write into the cgroup
+	pid := strconv.Itoa(os.Getppid())
+	must(ioutil.WriteFile(filepath.Join(mem, "ubutu/cgroup.procs"), []byte(pid), 0700))
+}
+
+
+// ERR HANDLING
 func must(err error)  {
 	if err != nil {
 		panic(err)
